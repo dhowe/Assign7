2d21eb976e8239c458b86125c77e31dedd8225ce
dhowe <dhowe@cims.nyu.edu>
2022-03-11 17:19:42 +0800

    fixed bug

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Assign7
-The is an example repo for Assignment #7
+The is an example repository for Assignment #7
 
 Here is a [demo](https://dhowe.github.io/Assign7/)
 
--- sketch.js
+++ sketch.js
@@ -7,30 +7,30 @@ function setup() {
   generate();
 }
 
-function generate(minLen=50, maxLen=120) {
-  
+function generate(minLen = 50, maxLen = 200) {
+
   // pick a random starting seq
   let result = random(Object.keys(model)
     .filter(k => /^[A-Z]/.test(k)));
-  
+
   // loop until we have our length
   while (result.length < maxLen) {
 
     // get the last N-1 chars
-    let seq = result.slice(-(N-1));
-    
+    let seq = result.slice(-(N - 1));
+
     // look it up the model
     let options = model[seq];
-    
+
     // pick a random next char
     let next = random(options);
     result += next; // add it to our string
-    
+
     if (result.length >= minLen && result.endsWith('.')) break;
   }
-  
+
   let span = document.getElementById('content');
-  span.innerText += result;  
+  span.innerText += ' ' + result;
 }
 
 
